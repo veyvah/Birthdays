9747102e4eb4446c876e4304e504d04c5e2db7a0
ava <ava.cg.lee@gmail.com>
2025-08-08 10:48:26 -0400

    bro im so done

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,17 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var friends: [friend] = [
+        friend(name: "Joan", birthday: .now),
+        friend(name: "Maya", birthday:
+                Date(timeIntervalSince1970: 0))
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            
+            
+            List(friends, id: \.name) {friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format:
+                            .dateTime.month(.wide)
+                            .day().year())
+                }
+            }
+            
+        }.navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing:20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(displayComponents, selection:
+                                $newBirthday,
+                               in: Date.distantPast...Date
+                        .now, displayedComponents:
+                            .date,selection,;
+                               $newBirthday;,
+                               in: Date.distantPast...Date
+                        .now,
+                               TextField("Name", text: $newName)
+                        .textFieldStyle(.roundedBorder)
+                    )
+                    Button("save") {
+                        let newFriend = friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                    }
+                    .bold()
+                }
+            }
     }
-}
-
-#Preview {
-    ContentView()
+    
+    
+    #Preview {
+        ContentView()
+    }
+    
+    
 }
--- Birthdays/friend.swift
+++ Birthdays/friend.swift
@@ -10,6 +10,7 @@ import Foundation
 class friend {
     var name: String
     var birthday: Date
+    
     init(name: String, birthday:Date) {
         self.name = name
         self.birthday = birthday
