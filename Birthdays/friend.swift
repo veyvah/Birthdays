f169bb74f10cd24a0a874b7e52b16e3603eccf3c
ava <ava.cg.lee@gmail.com>
2025-08-08 09:52:20 -0400

    added birthday forfriend

---
+++ Birthdays/friend.swift
@@ -0,0 +1,17 @@
+//
+//  friend.swift
+//  Birthdays
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import Foundation
+
+class friend {
+    var name: String
+    var birthday: Date
+    init(name: String, birthday:Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
